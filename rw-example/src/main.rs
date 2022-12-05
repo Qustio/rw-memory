c245d5eed090dc9228a5de64991c66affdfe392f
Qustio <qustioof@gmail.com>
2022-12-05 12:35:53 +0300

    Adresses of bin-test added

--- rw-example/src/main.rs
+++ rw-example/src/main.rs
@@ -1,8 +1,8 @@
 use rw_memory::*;
 
 fn main() {
-    //let name = "bin-test.exe";
-    let name = "factorio.exe";
+    let name = "bin-test.exe";
+    //let name = "factorio.exe";
 
     let proc_id = get_proc_id_from_string(name.to_string());
     println!("Process ID: {:X}", proc_id);
@@ -12,10 +12,15 @@ fn main() {
     println!("Module base adress: 0x{:X}", module_base);
 
     // multi-popinters jumping
+    //let addr = jump(
+    //    h_process,
+    //    module_base,
+    //    &[0x01CC39F0, 0x60, 0x158, 0x60, 0x8, 0x98, 0x30, 0x30],
+    //);
     let addr = jump(
         h_process,
         module_base,
-        &[0x01CC39F0, 0x60, 0x158, 0x60, 0x8, 0x98, 0x30, 0x30],
+        &[0x000261E0, 0x18, 0x88, 0x830, 0x0, 0x0, 0x10, 0x5C4],
     );
     println!("Addr: 0x{:X}", addr);
 
@@ -31,6 +36,5 @@ fn main() {
         Ok(it) => it,
         Err(err) => println!("{}", err),
     };
-
     println!("{} -> {} + 10 = {}", v, v, v + 10);
 }
